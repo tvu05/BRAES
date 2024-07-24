5ebce1110467512091ef9b5f1bd9c1d3fcc0c8f8
tvu05 <thiennhi.vu@gmail.com>
2024-07-24 15:07:16 -0700

    Scopus_SpeciesResults

--- scripts/Scopus_TrialOne.R
+++ scripts/Scopus_TrialOne.R
@@ -1,5 +1,6 @@
 #Author: Thien-Nhi Vu 
 #Date Created: 7/21/24
+#Date updated: 7/24/24
 
 # Sample working script that queries Scopus API
 # for a list of bird names and associated roost terms
@@ -23,10 +24,11 @@ xlSpe <- read_xlsx("data/Chapter_2_PhD_data_final.xlsx",
 speciesList <- xlSpe[,"Species1", drop = TRUE]
 rm(xlSpe)
 # replace spaces with encoding
-speciesList <- gsub(" ", "%20",speciesList)
+birdName <- gsub(" ", "%20",speciesList)
 # subset the data for testing
 # DELETE WHEN TESTING WORKS
-speciesListTest <- speciesList[1:40]
+birdName <- birdName[1:8374]
+print(birdName)
 
 
 ###
@@ -36,14 +38,14 @@ speciesListTest <- speciesList[1:40]
 # base elements
 url <- "https://api.elsevier.com/content/search/scopus?"
 query_TAK <- "query=TITLE-ABS-KEY('"
-abstSearch <- "filter=title_and_abstract.search:"
+#abstSearch <- "filter=title_and_abstract.search:"
 # roost terms
 roostTerms <- "'%20AND%20(roost%20OR%20roosting%20OR%20communally%20OR%20communal))&apiKey="
 #how do include my API key without writing it in my script
-apiKey <- "API"
+apiKey <- "133f1733e7e9432738a56c5d683abb63"
 
 build_Search <- function(url, query_TAK, birdName, roostTerms, apiKey){
-  search <- paste0(url, query_TAK, "Accipiter%20albogularis",roostTerms, apiKey)
+  search <- paste0(url, query_TAK, birdName,roostTerms, apiKey)
   request <- httr2::request(search)
   resp <-req_perform(request)
   body <- fromJSON(rawToChar(resp$body))
@@ -51,11 +53,18 @@ build_Search <- function(url, query_TAK, birdName, roostTerms, apiKey){
   return(countResults)
 }
 
+count_of_articles <- vector(mode = "numeric", length = length(birdName))
 
-birdName <- xlSpe[1:4,1, drop = T]   #drop(boolean) drops table: left w/ only a vector 
-birdName <- gsub(" ", "%20",birdName)
 
-for(i in length(birdName)){
-  print(birdName)
+for(i in 1:length(birdName)){
+  print(birdName[i])
+  print(i)
+  results <- build_Search(url, query_TAK, birdName[i], roostTerms, apiKey)
+  count_of_articles[i] <- results
 }
+
+wrap_up <- data.frame(Species = gsub("%20", " ", birdName),
+                      Results = count_of_articles)
+
+write.csv(wrap_up, "outputs/output_Scopus.csv", row.names = FALSE)
 
